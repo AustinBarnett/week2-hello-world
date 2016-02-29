8272fcd1360fc1e5538fea510014fb3f45e592a1
Austin Barnett <abarnett10@mail.valenciacollege.edu>
2016-02-28 20:13:54 -0500

    Did project.
    Austin Barnett

---
+++ Hello World Mini Project Barnett.py
@@ -0,0 +1,16 @@
+
+print('Hello World')
+print('What is your favortie language?') #ask what their favorite language is
+print('1.) Spanish')
+print('2.) French')
+print('3.) German') 
+language = input ()
+if language == 'Spanish':
+    print ('Hola amigo.')
+if language == 'French':
+    print ('Salut l ami')
+if language == 'German':
+    print ('Hallo Freund')
+print ('Exit')
+
+
